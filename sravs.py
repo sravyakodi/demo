6e4cba880f3ef93944c495ad2b874909e6989e6a
sravyakodi <133135698+sravyakodi@users.noreply.github.com>
2024-01-12 10:32:58 +0530

    Create sravs.py

---
+++ sravs.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('puppy')
+st.write('puppy')
+st.image('https://resize.indiatvnews.com/en/resize/newbucket/1080_-/2023/05/bts-kim-taehyung-pics-1684124471.jpg',caption='oppa')
